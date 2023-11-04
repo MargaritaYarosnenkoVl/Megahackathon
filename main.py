b72810df60ff4a4f5a208472e3a38ef5dcfe75e4
bushuev-alex <bushuev-alex@mail.ru>
2023-11-05 01:09:33 +0700

    1. добавил роутеры авторизации и регистрации в main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi_users import fastapi_users, FastAPIUsers
+
+from auth.auth import auth_backend
+from auth.db import User
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
 
 app = FastAPI(
     title="This is ХОРОШО!"
 )
 
+fastapi_users = FastAPIUsers[User, int](get_user_manager,
+                                        [auth_backend]
+                                        )
 
-@app.get("/")
-def hello():
-    return "Hello world"
+app.include_router(fastapi_users.get_auth_router(auth_backend),
+                   prefix="/auth/jwt",
+                   tags=["auth"],
+                   )
+
+app.include_router(fastapi_users.get_register_router(UserRead, UserCreate),
+                   prefix="/auth",
+                   tags=["auth"]
+                   )
