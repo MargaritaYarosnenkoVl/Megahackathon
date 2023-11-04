863a4a1c6f592372327a10037a13ff7f6e248da2
bushuev-alex <bushuev-alex@mail.ru>
2023-11-05 00:37:39 +0700

    1. добавил новые поля к таблице user

--- models/models.py
+++ models/models.py
@@ -27,9 +27,12 @@ role = Table("role",
 user = Table("user",
              metadata,
              Column("id", Integer, primary_key=True),
-             Column("email", String, nullable=False),
              Column("username", String, nullable=False),
-             Column("password", String, nullable=False),
+             Column("hashed_password", String(length=1024), nullable=False),
              Column("registred_at", TIMESTAMP, default=datetime.utcnow),
-             Column("role_id", Integer, ForeignKey(role.c.id))
+             Column("role_id", Integer, ForeignKey(role.c.id)),
+             Column("email", String(length=320), unique=True, index=True, nullable=False),
+             Column("is_active", Boolean, default=True, nullable=False),
+             Column("is_superuser", Boolean, default=False, nullable=False),
+             Column("is_verified", Boolean, default=False, nullable=False)
              )
