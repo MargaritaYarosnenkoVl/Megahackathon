88e79c224e000b94198fba1615772354c811af6e
bushuev-alex <bushuev-alex@mail.ru>
2023-11-12 02:09:22 +0700

    1. update schemas.py  в auth

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -19,7 +19,7 @@ class UserCreate(schemas.BaseUserCreate):
     username: str
     email: EmailStr
     password: str
-    role_id: int
+    role_id: int = 1
     is_active: Optional[bool] = True
     is_superuser: Optional[bool] = False
     is_verified: Optional[bool] = False
