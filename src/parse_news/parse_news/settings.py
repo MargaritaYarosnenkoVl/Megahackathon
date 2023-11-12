394105e461ee6f9a55a61af09f9e947bf35b4a45
bushuev-alex <bushuev-alex@mail.ru>
2023-11-12 21:19:32 +0700

    1. изменил settings.py парсеров на ROBOTS_OBEY=False

--- src/parse_news/parse_news/settings.py
+++ src/parse_news/parse_news/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "parse_news.spiders"
 #USER_AGENT = "parse_news (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
