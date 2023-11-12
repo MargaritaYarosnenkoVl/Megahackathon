e16e7081a5a3670bbc0d49ff7b141f33c8ffd0b4
bushuev-alex <bushuev-alex@mail.ru>
2023-11-12 23:35:42 +0700

    1. изменил endpoint router в parse_news

--- src/parse_news/router.py
+++ src/parse_news/router.py
@@ -6,17 +6,17 @@ from src.database import get_async_session
 from src.parse_news.models import article
 from src.parse_news.parse_news.spiders import naked_science_spider
 
-router = APIRouter(prefix="/add_naked_science_news",
+router = APIRouter(prefix="/get_filtered_news",
                    tags=["Add news"])
 
 
 @router.get("/")
-async def add_naked_science(session: AsyncSession = Depends(get_async_session)):
+async def get_filtered_news(session: AsyncSession = Depends(get_async_session)):
     # stmt = insert(article).values()
     # await session.execute(stmt)
     # await session.commit()
-    return
+    return "Coming soon"
 
 
 if __name__ == "__main__":
-    add_naked_science()
+    get_filtered_news()
