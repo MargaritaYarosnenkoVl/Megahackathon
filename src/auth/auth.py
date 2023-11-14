30c2ea864c01419763de686a50ff2ac98aa60a4b
bushuev-alex <bushuev-alex@mail.ru>
2023-11-15 02:04:15 +0700

    1. change jwt algo from rs to hs256

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -7,8 +7,7 @@ cookie_transport = CookieTransport(cookie_name="news", cookie_max_age=3600)
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=SECRET,
-                       lifetime_seconds=3600,
-                       algorithm="RS256")
+                       lifetime_seconds=3600)
 
 
 auth_backend = AuthenticationBackend(name="jwt",
