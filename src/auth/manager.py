28ba337ae4a30a33b2489bbfb932496863e7f2d2
bushuev-alex <bushuev-alex@mail.ru>
2023-11-15 01:28:04 +0700

    1. изменил ключи для верификации и сброса пароля в manager.py config.py

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -6,12 +6,12 @@ from fastapi_users import BaseUserManager, IntegerIDMixin
 
 from src.auth.models import User, get_user_db
 
-from src.config import SECRET
+from src.config import SECRET_AUTH
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
-    reset_password_token_secret = SECRET
-    verification_token_secret = SECRET
+    reset_password_token_secret = SECRET_AUTH
+    verification_token_secret = SECRET_AUTH
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
