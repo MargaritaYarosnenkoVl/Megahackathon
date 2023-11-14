2f62ae1be3ad91a5fe5685c578c4642bd4be1d68
bushuev-alex <bushuev-alex@mail.ru>
2023-11-15 02:58:49 +0700

    1. add token_audience to JWTstrat

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -7,7 +7,8 @@ cookie_transport = CookieTransport(cookie_name="news", cookie_max_age=3600)
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=SECRET,
-                       lifetime_seconds=3600)
+                       lifetime_seconds=3600,
+                       token_audience=["*"])
 
 
 auth_backend = AuthenticationBackend(name="jwt",
