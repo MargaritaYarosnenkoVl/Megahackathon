432b69fddfaa1a72ffb856e74877bbcf6cc0bd58
bushuev-alex <bushuev-alex@mail.ru>
2023-11-15 01:34:51 +0700

    1. add class Config to UserRead in schemas.py auth

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -14,6 +14,9 @@ class UserRead(schemas.BaseUser[int]):
     is_superuser: bool = False
     is_verified: bool = False
 
+    class Config:
+        orm_mode = True
+
 
 class UserCreate(schemas.BaseUserCreate):
     username: str = "user"
