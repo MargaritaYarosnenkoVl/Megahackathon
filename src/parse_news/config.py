90d0f722426348539b9b87be1f92ead4db322108
bushuev-alex <bushuev-alex@mail.ru>
2023-11-27 00:36:49 +0700

    add config with env variables to parse_news project

---
+++ src/parse_news/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FSTR_DB_LOGIN = os.getenv('FSTR_DB_LOGIN')
+FSTR_DB_PASS = os.getenv('FSTR_DB_PASS')
+FSTR_DB_HOST = os.getenv('FSTR_DB_HOST')
+FSTR_DB_PORT = os.getenv('FSTR_DB_PORT')
+FSTR_DB_NAME = os.getenv('FSTR_DB_NAME')
+
