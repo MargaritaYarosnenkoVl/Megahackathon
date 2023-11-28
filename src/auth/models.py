0fd871020a400cae5464413b4f75e02a5674fb99
bushuev-alex <bushuev-alex@mail.ru>
2023-11-28 21:35:30 +0700

    set role column name primary_key=true

--- src/auth/models.py
+++ src/auth/models.py
@@ -18,7 +18,7 @@ metadata = MetaData()
 role = Table("role",
              metadata,
              Column("id", Integer, primary_key=True),
-             Column("name", String, nullable=False),
+             Column("name", String, nullable=False, unique=True, primary_key=True),
              # Column("permissions", JSON)
              )
 
@@ -31,7 +31,7 @@ user = Table("user",
              Column("username", String, nullable=False, unique=True),
              Column("hashed_password", String(length=1024), nullable=False),
              Column("registred_at", TIMESTAMP, default=datetime.utcnow),
-             Column("role_id", Integer, ForeignKey(role.c.id)),
+             Column("role_name", Integer, ForeignKey(role.c.id)),
              Column("email", String(length=320), unique=True, index=True, nullable=False),
              Column("is_active", Boolean, default=True, nullable=False),
              Column("is_superuser", Boolean, default=False, nullable=False),
